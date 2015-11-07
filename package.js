5d23d04506bd4d37480df3b17f9e679322cad8a3
Christian Grlica <christian@heavenseven.net>
2015-11-07 15:55:19 +0100

    update packages, meteor v1.2

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'heaven7:wsl-alert',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Wrapper for juliancwirko:s-alert',
   git: 'https://github.com/heaven7/wsl-alert.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.1.0.2');
+    api.versionsFrom('1.2');
     api.addFiles('wsl-alert.js');
 
-    api.use('juliancwirko:s-alert@2.4.1');
+    api.use('juliancwirko:s-alert@3.1.2');
     api.imply('juliancwirko:s-alert');
 
     api.export('wAlert', ['client'])
