90b4c50dfdbc680f39bb4387b05304f99b1b0598
Christian Grlica <christian@heavenseven.net>
2016-04-22 13:19:55 +0200

    Release 0.0.4

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'heaven7:wsl-alert',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Wrapper for juliancwirko:s-alert',
   git: 'https://github.com/heaven7/wsl-alert.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.2');
-    api.addFiles('wsl-alert.js');
+    api.versionsFrom('1.2')
+    api.addFiles('wsl-alert.js')
 
-    api.use('juliancwirko:s-alert@3.1.2');
-    api.imply('juliancwirko:s-alert');
+    api.use('juliancwirko:s-alert@3.1.4')
+    api.imply('juliancwirko:s-alert')
 
     api.export('wAlert', ['client'])
 
-});
+})
